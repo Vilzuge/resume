6b4194c06af0acc992d052c068c4edb52fed7afb
Ville Martas <ville.martas@hotmail.com>
2022-01-28 01:27:29 +0200

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -27,6 +27,7 @@ class Portfolio extends Component {
       <section id="portfolio">
         <div className="row">
           <div className="twelve columns collapsed">
+              <h1>Projects</h1>
               <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
               </div>
